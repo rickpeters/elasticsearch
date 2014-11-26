29b639d60ea667e822bf3e10696481d59ac280b6
peterr1 <rick.peters@kadaster.nl>
2014-11-26 14:29:04 +0100

    Working configuration with integrated Marvel dashboard

--- prepare_container.sh
+++ prepare_container.sh
@@ -12,10 +12,19 @@ do
     # Control will enter here if $DIRECTORY doesn't exist.
     mkdir $DIRECTORY
   fi
+
+  # install marvel plugin
+  mkdir -p $DIRECTORY/plugins/marvel
+  unzip -o marvel-latest.zip -d $DIRECTORY/plugins/marvel
+  
+  pushd .
   cd $DIRECTORY
   #if [ ! -f "elasticsearch.yml" ]; then
     printf "path:\n logs: /data/log\n data: /data/data" > elasticsearch.yml
+    printf "\n plugins: /data/plugins" >> elasticsearch.yml
     printf "\nhttp.cors.allow-origin: "/.*/"\nhttp.cors.enabled: true" >> elasticsearch.yml
     printf "\ncluster:\n name: gimabigdata" >> elasticsearch.yml
   #fi
+  popd
+  
 done
